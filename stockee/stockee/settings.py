d78bac94e8498628be016cda461462d53bfe7245
farouk-elouassif <elouassiffarouk@gmail.com>
2026-03-05 23:38:05 +0000

    Merge fixed

--- stockee/stockee/settings.py
+++ stockee/stockee/settings.py
@@ -27,10 +27,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
-<<<<<<< HEAD
-=======
-
->>>>>>> prod
 
 # Application definition
 
